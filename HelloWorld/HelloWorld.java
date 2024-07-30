ca9421f91c1752f17018a4ae257bbd71c921b9cf
GamerNoTitle <GamerNoTitle@outlook.com>
2024-07-30 17:01:01 +0800

    init

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * HelloWorld Program is the basic program in learning Java
+ * @author GamerNoTitle
+ * @version 1.0
+ */
+
+public class HelloWorld {
+    /**
+     * This is the main method of the program
+     * @param args This is the args of the program
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
